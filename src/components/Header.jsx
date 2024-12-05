8db3c7dd98046f3c7991f3b1a39c034583a40773
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2024-12-05 13:04:02 +0100

    Travel Journal - Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <img
+        className="globe-img"
+        src="./src/assets/Globe.png"
+        alt="globe picture"
+      />
+      <h1>my travel journal.</h1>
+    </header>
+  );
+}
